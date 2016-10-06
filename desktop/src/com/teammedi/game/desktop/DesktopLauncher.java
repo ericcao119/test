8eeb2716b69c94a2cd3f089ab11c0389fcb38315
ericcao119 <eric7252000@gmail.com>
2016-10-05 21:20:41 -0400

    Initial Commit
    Commit 1

---
+++ desktop/src/com/teammedi/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.teammedi.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.teammedi.game.MediGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MediGame(), config);
+	}
+}
